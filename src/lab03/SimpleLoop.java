79711dc249fb5c0d183ac2cd63c017408ed9fb86
Enrique Pesantez <enrique.pesantez1@marist.edu>
2020-02-10 11:22:25 -0500

    Final Version of Simple Loop

---
+++ src/lab03/SimpleLoop.java
@@ -0,0 +1,11 @@
+package lab03;
+
+public class SimpleLoop {
+    public static void main(String[] args){
+        // For loop for printing each number 0 through 50
+        for(int i = 0; i <= 50; i++){
+            // Print statement for each number i
+            System.out.println(i);
+        }
+    }
+}
